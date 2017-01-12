b943b5d0766f21c62df9bd70d2d9d0e731adc244
MichaelMini <mcho911@gmail.com>
2017-01-12 06:44:05 +0000

    Final

--- tax.js
+++ tax.js
@@ -22,42 +22,53 @@ var companySalesData = [
   }
 ];
 
-var eachComp = {
-    name: "Telus",
-    province: "BC",
-    sales: [ 100, 200, 400 ]
-  };
+function branchTax(thisBranch, salesTaxRates) {
+    var areaRate = salesTaxRates[thisBranch.province];
+    var areaTaxTotal = addSales(thisBranch) * areaRate;
+    return areaTaxTotal;
+}
 
-
-function addSales(eachComp) {
-  // var sales = [1,2,3,4];
+function addSales(eachBranch) {
   var total=0;
-  // console.log(eachComp.sales.length);
-  for( var i = 0; i < eachComp.sales.length; i++) {
-    total += eachComp.sales[i];
-    // console.log(total);
+  for( var i = 0; i < eachBranch.sales.length; i++) {
+    total += eachBranch.sales[i];
   }
   return total;
 }
 
-console.log(addSales(eachComp));
-
-
-// function findComp(companySalesData) {
-//   for ( var i = 0; i < companySalesData.lenght; i++) {
-
-// }
-
-
-//   companySalesData.sales[i]
-// }
-
-function calculateSalesTax(salesData, taxRates) {
-  // Implement your code here
-
+function calculateSalesTax(companySalesData, salesTaxRates) {
+  var answer = {};
+  for ( var i = 0; i < companySalesData.length; i++) {
+    var thisBranch = companySalesData[i];
+    var branchTotalSales = addSales(thisBranch);
+    var branchTotalTaxes = branchTax(thisBranch, salesTaxRates);//doTaxes();
+    if (!answer[thisBranch.name]) {
+      answer[thisBranch.name] = {
+        totalSales: branchTotalSales,
+        totalTaxes: branchTotalTaxes
+      }
+    } else {
+      answer[thisBranch.name].totalSales += branchTotalSales;
+      answer[thisBranch.name].totalTaxes += branchTotalTaxes;
+    }
+  }
+  return answer;
 }
 
+
+// function eachCompSales(companySalesData) {
+//   for ( var i = 0; i < companySalesData.length; i++) {
+//     var thisComp = companySalesData[i];
+//     var branchSalesTotal = addSales(thisComp);
+//     console.log("addSales", addSales(thisComp));
+//   }
+// }
+
+
+
+
 var results = calculateSalesTax(companySalesData, salesTaxRates);
+console.log(results);
 
 /* Expected Results:
 {
